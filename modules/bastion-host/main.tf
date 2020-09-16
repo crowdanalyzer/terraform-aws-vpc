c8f401da50a4eab5072d44ff237df37f9141ff51
Bahaa Galal <bahaa.g.ali@gmail.com>
2020-09-16 11:19:17 +0200

    use protocol all instead of -1 for readability

--- modules/bastion-host/main.tf
+++ modules/bastion-host/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group_rule" "allow_all_outbound_security_group_rule" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
-  protocol    = "-1"
+  protocol    = "all"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.security_group.id
