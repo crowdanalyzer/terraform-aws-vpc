81fbb0281f005f09109252568efa1acf28ff5c59
Aabed <ahmad.aabed.m@gmail.com>
2020-09-29 16:26:01 +0200

    Adds comment for hosted zone

--- modules/route53-zone/main.tf
+++ modules/route53-zone/main.tf
@@ -1,9 +1,10 @@
-resource "aws_route53_zone" "hosted_zone" {
+resource "aws_route53_zone" "route53_zone" {
+  comment = "Private hosted zone for VPCs created by Terraform."
   name = var.domain_name
   dynamic "vpc" {
     for_each = var.vpcs
     content {
-      vpc_id = vpc.value.vpc_id
+      vpc_id = vpc.value
     }
   }
 }
