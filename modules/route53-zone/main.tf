8f6031955fc53b2e7cfd883a2a3fb3d19acccf78
Bahaa Galal <bahaa.g.ali@gmail.com>
2020-10-05 12:00:06 +0200

    update aws_route53_zone to use the new vpc variable

--- modules/route53-zone/main.tf
+++ modules/route53-zone/main.tf
@@ -1,10 +1,17 @@
+# ------------------------------------------------------------------------------------------------------------------
+# CREATE PRIVATE ROUTE53 ZONE
+# ------------------------------------------------------------------------------------------------------------------
+
 resource "aws_route53_zone" "route53_zone" {
-  comment = "Private hosted zone for VPCs created by Terraform."
-  name = var.domain_name
+  name    = var.domain_name
+  comment = "${var.domain_name} private hosted zone."
+
   dynamic "vpc" {
-    for_each = var.vpcs
+    for_each = { for v in var.vpcs : v.id => v.region }
+
     content {
-      vpc_id = vpc.value
+      vpc_id     = vpc.key
+      vpc_region = vpc.value
     }
   }
 }
